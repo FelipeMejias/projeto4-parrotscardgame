a78d9429787589ee713b079f499082f09f644d21
felipemejias <felipe.mejias50@gmail.com>
2022-02-05 15:12:14 -0300

    embaralhar cartas+jogo acontecendo

--- script.js
+++ script.js
@@ -7,35 +7,69 @@ function perguntarQuantidade(){
     quantidadeCartas=quantidade
 
 }
-perguntarQuantidade();
+
 
 const container=document.querySelector(".container")
 
 function adicionarCartas(){
 
     for(let contador=0;contador<quantidadeCartas;contador++){
-        container.innerHTML+='<div class="card" onClick="virar(this)"><div class="face front-face"><img src="papagaio.png"></div><div class="face back-face">"bekjcwkec"</div></div>'
+        container.innerHTML+='<div class="card" onClick="virar(this,'+listaJogo[contador].nome+')"><div class="face front-face"><img src="papagaio.png"></div><div class="face back-face">'+listaJogo[contador].imagem+'</div></div>'
 
     }
 }
 
-adicionarCartas();
+
 
 let contador=0
-function virar(cartaClicada){
-    if(contador<2){
+let nomeCarta1=''
+let nomeCarta2=''
+let carta1=null;
+let carta2=null
+function virar(cartaClicada,nomeCarta){
+    if(contador===0){
+
+        if(cartaClicada.classList.contains('virada')){
+            alert('escolha outra');
+        }
+        else{    
     cartaClicada.classList.add('virada');
-    contador++;
+    nomeCarta1=nomeCarta;
+    carta1=cartaClicada;
+    contador++}}
+    
+    else if(contador===1){
+
+        if(cartaClicada.classList.contains('virada')){
+            alert('escolha outra');
+        }
+        else{cartaClicada.classList.add('virada');
+        nomeCarta2=nomeCarta;
+        carta2=cartaClicada;
+        contador++;
+        conferirPar(nomeCarta1,nomeCarta2);
+        }
+    }   
+}
+function zerarContador(){contador=0;}
+
+function desvirar(){
+    carta1.classList.remove('virada');
+    carta2.classList.remove('virada');
+}
+
+function conferirPar(primeiraCarta,segundaCarta){
+    if(primeiraCarta==segundaCarta){
+        zerarContador();
+        nomeCarta1='';
+        nomeCarta2='';
     }
     else{
-        contador=0;
-        desvirar();
+        setTimeout(desvirar,1000);
+        setTimeout(zerarContador,1000);
     }
 }
 
-function desvirar(){
-    const cartaViradas=document.querySelector('.virada');
-    cartaViradas.classList.remove('virada');
-    const outraCarta=document.querySelector('.virada');
-    outraCarta.classList.remove('virada');
-}
+
+
+
---
+++ script2.js
@@ -0,0 +1,35 @@
+
+const michael={nome:'michael', imagem:'<img src="michael.jpg">'};
+const dwight={nome:'dwight', imagem:'<img src="dwight.jpg">'};
+const pam={nome:'pam', imagem:'<img src="pam.jpg">'};
+const jim={nome:'jim', imagem:'<img src="jim.jpg">'};
+const holly={nome:'holly', imagem:'<img src="holly.jpg">'};
+const stanley={nome:'stanley', imagem:'<img src="stanley.jpg">'};
+const kevin={nome:'kevin', imagem:'<img src="kevin.jpg">'};
+
+
+const listaTodos=[michael,michael,dwight,dwight,pam,pam,jim,jim,holly,holly,stanley,stanley,kevin,kevin];
+
+const listaJogo=[];
+
+function formarListaJogo(){
+    for(let i=0;i<quantidadeCartas;i++){
+        const item=listaTodos[i];
+        listaJogo.push(item);
+    }
+    listaJogo.sort(comparador);
+}
+
+
+
+
+
+function comparador() { 
+	return Math.random() - 0.5; 
+}
+
+
+
+perguntarQuantidade();
+formarListaJogo();
+adicionarCartas();
